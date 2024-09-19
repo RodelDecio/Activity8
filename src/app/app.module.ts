e3ee63435d5c4a8df5e7f36626ee729ec6a7beb2
RodelDecio <rodeldecio068@gmail.com>
2024-09-19 20:26:43 +0800

    #11evenoddcheckerrr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import {UsergreetingComponent} from "./components/usergreeting/usergreeting.comp
 import {CalculatorComponent} from "./components/calculator/calculator.component";
 import {TextlengthComponent} from "./components/textlength/textlength.component";
 import {CurrencyconverterComponent} from "./components/currencyconverter/currencyconverter.component";
+import {EvenoddcheckerComponent} from "./components/evenoddchecker/evenoddchecker.component";
 
 const routes: Routes =[
   {path:'',component: DisplayhelloworldComponent},
@@ -27,6 +28,7 @@ const routes: Routes =[
   {path: 'calculator',component:CalculatorComponent},
   {path: 'textlength',component:TextlengthComponent},
   {path: 'currencyconverter',component:CurrencyconverterComponent},
+  {path: 'evenoddchecker',component:EvenoddcheckerComponent},
 
 ]
 
@@ -51,6 +53,7 @@ const routes: Routes =[
     CalculatorComponent,
     TextlengthComponent,
     CurrencyconverterComponent,
+    EvenoddcheckerComponent,
 
 
   ],
