fff5d87cf08bbf7a50b04c37386e2d2228886e35
RodelDecio <rodeldecio068@gmail.com>
2024-09-19 22:04:50 +0800

    #19charactercounter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import {MultiplicationtableComponent} from "./components/multiplicationtable/mul
 import {SimpleloginComponent} from "./components/simplelogin/simplelogin.component";
 import {FahrenheittocelsiusComponent} from "./components/fahrenheittocelsius/fahrenheittocelsius.component";
 import {BookmarklistComponent} from "./components/bookmarklist/bookmarklist.component";
+import {CharactercounterComponent} from "./components/charactercounter/charactercounter.component";
 
 const routes: Routes =[
   {path:'',component: DisplayhelloworldComponent},
@@ -43,6 +44,7 @@ const routes: Routes =[
   {path: 'simplelogin',component: SimpleloginComponent},
   {path: 'fahrenheittocelsius',component: FahrenheittocelsiusComponent},
   {path: 'bookmarklist',component: BookmarklistComponent},
+  {path: 'charactercounter',component: CharactercounterComponent},
 
 
 ]
@@ -76,6 +78,7 @@ const routes: Routes =[
     SimpleloginComponent,
     FahrenheittocelsiusComponent,
     BookmarklistComponent,
+    CharactercounterComponent,
 
 
   ],
