1daf1c636358e48b1365a11363e90d700fbdd60b
RodelDecio <rodeldecio068@gmail.com>
2024-09-19 21:57:11 +0800

    #18bookmarklist

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import {ShowusernameComponent} from "./components/showusername/showusername.comp
 import {MultiplicationtableComponent} from "./components/multiplicationtable/multiplicationtable.component";
 import {SimpleloginComponent} from "./components/simplelogin/simplelogin.component";
 import {FahrenheittocelsiusComponent} from "./components/fahrenheittocelsius/fahrenheittocelsius.component";
+import {BookmarklistComponent} from "./components/bookmarklist/bookmarklist.component";
 
 const routes: Routes =[
   {path:'',component: DisplayhelloworldComponent},
@@ -41,6 +42,7 @@ const routes: Routes =[
   {path: 'multiplicationtable',component: MultiplicationtableComponent},
   {path: 'simplelogin',component: SimpleloginComponent},
   {path: 'fahrenheittocelsius',component: FahrenheittocelsiusComponent},
+  {path: 'bookmarklist',component: BookmarklistComponent},
 
 
 ]
@@ -73,6 +75,7 @@ const routes: Routes =[
     MultiplicationtableComponent,
     SimpleloginComponent,
     FahrenheittocelsiusComponent,
+    BookmarklistComponent,
 
 
   ],
